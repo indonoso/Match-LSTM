9f4276fd4d6f2ba86c56c9425c0341d667a12996
Ivania Donoso <ivania.donoso@gmail.com>
2021-07-14 11:59:12 -0400

    Added setup file for installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='machine_comprehension',
+    packages=['machine_comprehension'],
+    version='0.1.0',
+    description='Machine Comprehension models in PyTorch',
+    author='@laddie132 refactored by @indonoso',
+    requirements=['torch', 'numpy', 'spacy', 'h5py', 'pandas', 'yaml']
+)
