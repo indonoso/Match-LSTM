eb4c0d8a06e83dfc9e140b9321a798b6a208931e
Ivania Donoso <ivania.donoso@gmail.com>
2021-07-16 16:21:39 -0400

    Added KG processor

---
+++ machine_comprehension/dataset/kg.py
@@ -0,0 +1,30 @@
+from .utils import aho_create_statemachine, aho_find_all
+
+
+class KGEmbeddings:
+    def __init__(self, patterns2id):
+        self.patterns2id = patterns2id
+        self.forest = aho_create_statemachine(self.patterns2id.keys())
+        self.len_patterns = {p: len(p.split(' ')) for p in self.patterns2id.leys()}
+
+    def process(self, text):
+        positions = aho_find_all(text.split(' '), self.forest)
+
+        # Check overlap
+        # Keep the one that starts first
+        next = -1
+        validated_positions = []
+
+        for start, pattern in positions:
+            if start > next:
+                next = start + self.len_patterns[pattern]
+                validated_positions.append((start, pattern))
+
+        # Get ids
+        # We repeat the KG embedding when it appears multiple times
+        ids = [0] * len(text)
+        for start, pattern in validated_positions:
+            size = self.len_patterns[pattern]
+            ids[start:start + size] = [self.patterns2id[pattern]] * size
+
+        return ids
--- machine_comprehension/dataset/preprocess_data.py
+++ machine_comprehension/dataset/preprocess_data.py
@@ -3,16 +3,16 @@
 
 __author__ = 'han'
 
-import zipfile
 import spacy
 import json
 import h5py
 import logging
 import numpy as np
+import pickle
 from functools import reduce
 from ..utils.functions import pad_sequences
 from .doc_text import DocText, Space
-
+from .kg import KGEmbeddings
 logger = logging.getLogger(__name__)
 
 
@@ -29,11 +29,13 @@ class PreprocessData:
 
     def __init__(self, global_config):
         # data config
+        # TODO check configs
         self._dev_path = ''
         self._train_path = ''
         self._export_squad_path = ''
         self._glove_path = ''
         self._embedding_size = 300
+        self._kg_embedding_size = 100
         self._ignore_max_len = 10000
         self._load_config(global_config)
 
@@ -45,15 +47,20 @@ class PreprocessData:
         self._char2id = {self.padding: 0, '`': 1}  # because nltk word tokenize will replace '"' with '``'
         self._pos2id = {self.padding: 0}
         self._ent2id = {self.padding: 0}
-        self._word2vec = {self.padding: [0. for i in range(self._embedding_size)]}
+        self._kg2id = {self.padding: 0}
+        self._word2vec = {self.padding: [0. for _ in range(self._embedding_size)]}
+        self._kg2vec = {self.padding: [0. for _ in range(self._embedding_size)]}
         self._oov_num = 0
 
         # data need to store in hdf5 file
-        self._meta_data = {'id2vec': [[0. for i in range(self._embedding_size)]],
+        self._meta_data = {'id2vec': [[0. for _ in range(self._embedding_size)]],
                            'id2word': [self.padding],
                            'id2char': [self.padding, '`'],
                            'id2pos': [self.padding],
-                           'id2ent': [self.padding]}
+                           'id2ent': [self.padding],
+                           'id2kg': [self.padding],
+                           'idkg2vec': [[0. for _ in range(self._embedding_size)]],
+                           }
         self._data = {}
         self._attr = {}
 
@@ -63,6 +70,8 @@ class PreprocessData:
             self._nlp.remove_pipe('tagger')
         if not self._use_ent:
             self._nlp.remove_pipe('ner')
+
+        self.kg_processor = KGEmbeddings(self._kg_patterns2id_path)
 
     def _load_config(self, global_config):
         """
@@ -75,6 +84,8 @@ class PreprocessData:
         self._dev_path = data_config['dataset']['dev_path']
         self._export_squad_path = data_config['dataset_h5']
         self._glove_path = data_config['embedding_path']
+        self._kg_embeddings_vec_path = data_config['kg_embeddings_vec_path']
+        self._kg_patterns2id_path = data_config['kg_patterns2id_path']
 
         self.preprocess_config = global_config['preprocess']
         self._ignore_max_len = self.preprocess_config['ignore_max_len']
@@ -83,8 +94,10 @@ class PreprocessData:
         self._use_ent = self.preprocess_config['use_ent']
         self._use_em = self.preprocess_config['use_em']
         self._use_em_lemma = self.preprocess_config['use_em_lemma']
+        self._use_kg = self.preprocess_config['use_kg']
 
         self._embedding_size = int(self.preprocess_config['word_embedding_size'])
+        self._kg_embedding_size = int(self.preprocess_config['kg_embedding_size'])
 
     def _read_json(self, path):
         """
@@ -120,7 +133,7 @@ class PreprocessData:
             cur_context = question_grp['context']
             cur_qas = question_grp['qas']
 
-            cur_context_doc = DocText(self._nlp, cur_context, self.preprocess_config)
+            cur_context_doc = DocText(self.kg_processor, self._nlp, cur_context, self.preprocess_config)
             if training and len(cur_context_doc) > self._ignore_max_len:  # some context token len too large
                 continue
 
@@ -136,7 +149,7 @@ class PreprocessData:
                 if self._use_char:
                     self._update_to_char(cur_question)
 
-                cur_question_doc = DocText(self._nlp, cur_question, self.preprocess_config)
+                cur_question_doc = DocText(self.kg_processor, self._nlp, cur_question, self.preprocess_config)
                 cur_question_ids = self._doctext_to_id(cur_question_doc)
 
                 # get em feature
@@ -179,8 +192,8 @@ class PreprocessData:
                                      "\nAnswer:" + cur_ans_text)
                         continue
 
-                    cur_ans_range_ids[(idx * 2):(idx * 2 + 2)] = np.array([pos_s, pos_e])
-                answers_range_wid.append(np.array(cur_ans_range_ids))
+                    cur_ans_range_ids[(idx * 2):(idx * 2 + 2)] = [pos_s, pos_e]
+                answers_range_wid.append(cur_ans_range_ids)
 
                 cnt += 1
                 if cnt % 100 == 0:
@@ -228,7 +241,7 @@ class PreprocessData:
         :return: word ids
         """
 
-        sentence = {'token': [], 'pos': [], 'ent': [], 'right_space': doc_text.right_space}
+        sentence = {'token': [], 'pos': [], 'ent': [], 'right_space': doc_text.right_space, 'kg': []}
 
         for i in range(len(doc_text)):
 
@@ -263,6 +276,22 @@ class PreprocessData:
                     self._meta_data['id2ent'].append(ent)
                 sentence['ent'].append(self._ent2id[ent])
 
+            if self._use_kg:
+                e = doc_text.kg[i]
+                if e not in self._kg2id:
+                    self._kg2id[e] = len(self._kg2id)
+                    self._meta_data['id2kg'].append(e)
+                # whether OOV
+                if e in self._kg2vec:
+                    self._meta_data['idkg2vec'].append(self._kg2vec[e])
+                else:
+                    self._oov_num += 1
+                    logger.debug('No.%d OOV kg %s' % (self._oov_num, e))
+                    self._meta_data['idkg2vec'].append([0. for _ in range(self._kg_embedding_size)])
+
+                sentence['kg'].append(self._kg2id[e])
+
+
         return sentence
 
     def _update_to_char(self, sentence):
@@ -295,6 +324,20 @@ class PreprocessData:
                     if word_num % 10000 == 0:
                         logger.info('handle word No.%d' % word_num)
 
+    def _handle_kg(self):
+        """
+        handle glove embeddings, restore embeddings with dictionary
+        :return:
+        """
+        with open(self._kg_embeddings_vec_path, 'r') as f:
+            for line in f:
+                line_split = line.split('\t')
+                self._kg2vec[line_split[0]] = np.asarray(line_split[1:], "float32")
+               
+    def save_pickle_object(self, filename):
+        with open(filename, 'wb') as output:  # Overwrites any existing file.
+            pickle.dump(self, output, pickle.HIGHEST_PROTOCOL)
+            
     def _export_squad_hdf5(self):
         """
         export squad dataset to hdf5 file
@@ -334,7 +377,6 @@ class PreprocessData:
                         data = sub_grp.create_dataset(subsub_key, subsub_value.shape, dtype=cur_dtype,
                                                       **self._compress_option)
                         data[...] = subsub_value
-
                 else:
                     cur_dtype = str_dt if sub_value.dtype.type is np.str_ else sub_value.dtype
                     data = data_grp.create_dataset(sub_key, sub_value.shape, dtype=cur_dtype,
