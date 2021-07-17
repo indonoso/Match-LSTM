2a08afef165a0f8801fa9335afee22b08edaaaa7
Ivania Donoso <ivania.donoso@gmail.com>
2021-07-17 03:12:06 -0400

    Updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='machine_comprehension',
-    packages=['machine_comprehension'],
+    packages=find_packages(),
+
     version='0.1.0',
     description='Machine Comprehension models in PyTorch',
     author='@laddie132 refactored by @indonoso',
